63a82fbfe4a83c220432462f2ccd9a3d8202343d
Matt Kettlewell <matt@kettlewell.net>
2022-08-10 20:56:42 -0500

    debugging saltstack update script

--- states/salt/files/update_saltstack.sh
+++ states/salt/files/update_saltstack.sh
@@ -2,11 +2,15 @@
 
 SALT_GIT_TAG={{ salt_git_tag }}
 EXPECTED_SALT_VERSION="salt ${SALT_GIT_TAG}"
+echo "SALT_GIT_TAG: ${SALT_GIT_TAG}"
+echo "EXPECTED_SALT_VERSION: ${EXPECTED_SALT_VERSION}"
 
 which salt > /dev/null 2>&1
 SALT_INSTALLED=$?
+echo "SALT_INSTALLED: ${SALT_INSTALLED}"
 if [[ ${SALT_INSTALLED} -eq 0 ]]; then
     SALT_VERSION=$(salt --version)
+    echo "SALT_VERSION: ${SALT_VERSION}"
     if [[ "${SALT_VERSION}" != "${EXPECTED_SALT_VERSION}" ]]; then
 	curl -s -L https://bootstrap.saltproject.io | sudo sh -s -- \
 							   -P \
--- states/salt/update_saltstack.sls
+++ states/salt/update_saltstack.sls
@@ -2,5 +2,6 @@ update_saltstack:
   cmd.script:
     - name: salt://salt/files/update_saltstack.sh
     - template: jinja
+    - shell: /bin/bash
     - context:
       salt_git_tag: {{ pillar['salt_git_tag'] }}
