453436fe116f847adb3872c5decde43a9dd019a6
Matt Kettlewell <matt@kettlewell.net>
2022-08-10 21:08:27 -0500

    cleaned up update_saltstack

--- states/salt/files/update_saltstack.sh
+++ states/salt/files/update_saltstack.sh
@@ -2,19 +2,15 @@
 
 SALT_GIT_TAG={{ salt_git_tag }}
 EXPECTED_SALT_VERSION={{ expected_salt_version }}
+SALT_VERSION=$(salt --version)
+
 echo "SALT_GIT_TAG: ${SALT_GIT_TAG}"
 echo "EXPECTED_SALT_VERSION: ${EXPECTED_SALT_VERSION}"
+echo "SALT_VERSION: ${SALT_VERSION}"
 
-which salt > /dev/null 2>&1
-SALT_INSTALLED=$?
-echo "SALT_INSTALLED: ${SALT_INSTALLED}"
-if [[ ${SALT_INSTALLED} -eq 0 ]]; then
-    SALT_VERSION=$(salt --version)
-    echo "SALT_VERSION: ${SALT_VERSION}"
-    if [[ "${SALT_VERSION}" != "${EXPECTED_SALT_VERSION}" ]]; then
-	curl -s -L https://bootstrap.saltproject.io | sudo sh -s -- \
+if [[ "${SALT_VERSION}" != "${EXPECTED_SALT_VERSION}" ]]; then
+    curl -s -L https://bootstrap.saltproject.io | sudo sh -s -- \
 							   -P \
 							   -q \
           git ${SALT_GIT_TAG}
-    fi
 fi
